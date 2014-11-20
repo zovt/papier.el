2e0b6b31b6b7e9602e127f4df48a6470b8c43833
ragesalmon <ragesalmon@gmail.com>
2014-11-20 14:35:27 -0500

    Initial commit.

---
+++ papier.el
@@ -0,0 +1,22 @@
+;;; papier.el --- A simple colorscheme.
+;; Author: Zovt <zovtly@gmail.com>
+;; URL: tbd
+;; Version: 0.1
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+(deftheme papier "Code on paper")
+
+;; Define colors
+(defvar papier-colors-alist
+  '(("papier-fg" . "#000000")
+    ("papier-bg" . "#FFFFFF"))
+  "List of papier colors.")
+
+
+(provide 'papier)
+;;; papier.el ends here
+
